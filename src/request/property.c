027993d270a29e27af1049cf375525deff73bded
tpoac <t.poac@cyim.com>
2022-03-22 14:22:08 +0000

    Set static properties OK

--- src/request/property.c
+++ src/request/property.c
@@ -24,17 +24,16 @@ char                *request_set_properties(
     cJSON           *property_json = NULL;
     cJSON           *pipeline_json = cJSON_GetObjectItemCaseSensitive(request_json, "pipeline");
     cJSON           *element_json = cJSON_GetObjectItemCaseSensitive(request_json, "element");
-    cJSON           *property_scalar_json = NULL;
     GParamSpec      *spec = NULL;
     // request get element
-    list_t          *deepblock = ast_deepblock_create(4, "document", "pipelines", pipeline_json->valuestring, "elements", element_json->valuestring);
+    list_t          *deepblock = ast_deepblock_create(5, "document", "pipelines", pipeline_json->valuestring, "elements", element_json->valuestring);
     ast_node_t      *element = ast_iblock_get(*root, deepblock);
-    GObjectClass    *objClass = G_OBJECT_GET_CLASS(element);
+    GObjectClass    *objClass = G_OBJECT_GET_CLASS(element->sdata->gstelement);
     GstCaps         *caps = NULL;
     char            *result_json_str = NULL;
     const char      *type_name = NULL;
 
-    if (element == NULL) {
+    if (element == NULL || element->sdata->gstelement == NULL) {
 
         /* Element unfound */
 
@@ -44,58 +43,56 @@ char                *request_set_properties(
 
         /* Element found, treat JSON properties each by one */
 
+        g_print("[REQUEST] Set properties\n");
+
         cJSON_ArrayForEach(property_json, properties_json) {
 
-            cJSON_ArrayForEach(property_scalar_json, property_json) {
+            /* Find correct property type */
 
-                /* Find correct property type */
+            spec = g_object_class_find_property(objClass, property_json->string);
+            if (spec != NULL) {
 
-                spec = g_object_class_find_property(objClass, property_scalar_json->string);
-                if (spec != NULL) {
+                type_name = g_type_name(spec->value_type);
+                if (strcmp(type_name, "gchararray") == 0) {
 
-                    type_name = g_type_name(spec->value_type);
-                    if (strcmp(type_name, "gchararray") == 0) {
+                    /* gchar * */
+                    g_object_set(G_OBJECT (element->sdata->gstelement), property_json->string, (gchar *)property_json->valuestring, NULL);
 
-                        /* gchar * */
-                        g_object_set(G_OBJECT (element), property_scalar_json->string, (gchar *)property_scalar_json->valuestring, NULL);
+                } else if (strcmp(type_name, "gfloat") == 0) {
 
-                    } else if (strcmp(type_name, "gfloat") == 0) {
+                    /* gfloat */
+                    g_object_set(G_OBJECT (element->sdata->gstelement), property_json->string, (gfloat)property_json->valuedouble, NULL);
 
-                        /* gfloat */
-                        g_object_set(G_OBJECT (element), property_scalar_json->string, (gfloat)property_scalar_json->valuedouble, NULL);
+                } else if (strcmp(type_name, "guint64") == 0) {
 
-                    } else if (strcmp(type_name, "guint64") == 0) {
+                    /* guint64 */
+                    g_object_set(G_OBJECT (element->sdata->gstelement), property_json->string, (guint64)property_json->valueint, NULL);
 
-                        /* guint64 */
-                        g_object_set(G_OBJECT (element), property_scalar_json->string, (guint64)property_scalar_json->valueint, NULL);
+                } else if (strcmp(type_name, "gboolean") == 0) {
 
-                    } else if (strcmp(type_name, "gboolean") == 0) {
+                    /* gboolean */
+                    g_object_set(G_OBJECT (element->sdata->gstelement), property_json->string, (gboolean)cJSON_IsTrue(property_json), NULL);
 
-                        /* gboolean */
-                        g_object_set(G_OBJECT (element), property_scalar_json->string, (gboolean)cJSON_IsTrue(property_scalar_json), NULL);
+                } else if (strcmp(type_name, "gint") == 0) {
 
-                    } else if (strcmp(type_name, "gint") == 0) {
+                    /* gint */
+                    g_object_set(G_OBJECT (element->sdata->gstelement), property_json->string, (gint)property_json->valueint, NULL);
 
-                        /* gint */
-                        g_object_set(G_OBJECT (element), property_scalar_json->string, (gint)property_scalar_json->valueint, NULL);
+                } else if (strcmp(type_name, "guint") == 0) {
 
-                    } else if (strcmp(type_name, "guint") == 0) {
+                    /* guint (maybe need to use stroul) */
+                    g_object_set(G_OBJECT (element->sdata->gstelement), property_json->string, (guint)property_json->valueint, NULL);
 
-                        /* guint (maybe need to use stroul) */
-                        g_object_set(G_OBJECT (element), property_scalar_json->string, (guint)property_scalar_json->valueint, NULL);
+                } else if (strcmp(type_name, "GstCaps") == 0) {
 
-                    } else if (strcmp(type_name, "GstCaps") == 0) {
+                    /* GstCaps */
+                    caps = (GstCaps *)gst_caps_from_string((gchar *)property_json->valuestring);
+                    g_object_set(G_OBJECT (element->sdata->gstelement), property_json->string, caps, NULL);
 
-                        /* GstCaps */
-                        caps = (GstCaps *)gst_caps_from_string((gchar *)property_scalar_json->valuestring);
-                        g_object_set(G_OBJECT (element), property_scalar_json->string, caps, NULL);
+                } else {
 
-                    } else {
-
-                        /* gint */
-                        g_object_set(G_OBJECT (element), property_scalar_json->string, (gint)property_scalar_json->valueint, NULL);
-
-                    }
+                    /* gint */
+                    g_object_set(G_OBJECT (element->sdata->gstelement), property_json->string, (gint)property_json->valueint, NULL);
 
                 }
 
