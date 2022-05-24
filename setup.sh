255d304c6ea1d8804b6f4a0242f450884bac78d3
tpoac <t.poac@cyim.com>
2022-05-24 14:53:41 +0200

    setup file, outside docker mode

---
+++ setup.sh
@@ -0,0 +1,14 @@
+# Ubuntu 22.04 LTS
+sudo apt update
+sudo apt install cmake
+sudo apt install git
+sudo apt install gcc
+sudo apt install g++
+sudo apt-get install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
+sudo apt install libyaml-dev
+sudo apt install libzmq3-dev
+sudo apt install libcurl4-nss-dev
+sudo apt install libcjson-dev
+mkdir build
+cd build
+cmake ..
